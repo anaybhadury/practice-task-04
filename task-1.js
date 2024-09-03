dda5ce2cce3292f1edcab6b592e9f7af0a6bf068
anaybhadury <bhadury9292@gmail.com>
2024-09-03 13:37:38 +0600

    added practice task 1

--- task-1.js
+++ task-1.js
@@ -1,4 +1,16 @@
 /**
  * -------------Practice Task-------------
- * 
+ * WRITE A LOOP 1 TO 200. USE BREAK TO EXIT THE LOOP ONCE YOU FIND 100.
  */
+
+let num = 1;
+
+while(num <= 200){
+    if(num === 100){
+        console.log('Found 100, exiting the loop.')
+        break;
+    }
+    console.log(num);
+    num++;
+}
+
