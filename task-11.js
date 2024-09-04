f33cb6a726db5ac5c1b4c79a20787c158f830237
anaybhadury <bhadury9292@gmail.com>
2024-09-04 12:41:37 +0600

    added practice task 11

---
+++ task-11.js
@@ -0,0 +1,12 @@
+/**
+ * -------------Practice Task-------------
+ * I WILL INVEST AT LEAST 6 HRS EVERY SINGLE DAY FOR NEXT 60 DAYS! THIS MESSAGE 60 TIMES. SO DISPLAY THIS.
+ */
+
+let hours = 6;
+
+while(hours <= 60){
+    console.log(hours);
+    hours++;
+    console.log('I will invest every single day 6 hrs a day')
+}
