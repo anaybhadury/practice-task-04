2809d70afc5ce92add87d09651fb0b06e032f00a
anaybhadury <bhadury9292@gmail.com>
2024-09-04 13:31:29 +0600

    added practice task 14

---
+++ task-14.js
@@ -0,0 +1,12 @@
+/**
+ * -------------Practice Task-------------
+ * AS ERSA IS LEARNNING NOW, SHE WANTS TO EXPLORE MORE AND MORE. TELL ERSA TO GENERATE A MULTIPLICATION TABLE FOR NUMBER 5.
+ */
+
+let num = 5;
+let i = 1;
+
+while(i <= 10){
+    console.log(num + ' x ' + i + ' = ' + (num * i));
+    i++;
+}
