52a274b9292dc993accc3a308678a6c970f6070c
anaybhadury <bhadury9292@gmail.com>
2024-09-04 11:21:24 +0600

    added practice task 7

---
+++ task-7.js
@@ -0,0 +1,12 @@
+/**
+ * -------------Practice Task-------------
+ * SUBTASK - 1
+ * FIND ALL THE ODD NUMBERS FROM 61 TO 100.
+ * 
+ * SUBTASK - 2
+ * FIND ALL THE EVEN NUMBERS FROM 78 TO 98
+ */
+
+for(let num = 61; num < 100; num = num+2){
+    console.log(num);
+}
