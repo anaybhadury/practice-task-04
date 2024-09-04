712cad588dcefcad8d2d84b232606994e31af4c3
anaybhadury <bhadury9292@gmail.com>
2024-09-04 12:01:41 +0600

    added practice task 9

---
+++ task.9.js
@@ -0,0 +1,42 @@
+/**
+ * -------------Practice Task-------------
+ * GENERATE A MULTIPICATION TABLE FOR NUMBER 1, 2, 8, 9, 10
+ */
+
+// let num = 1;
+// for(let i = 1; i <= 10; i++){
+//     console.log(num + ' x ' + i + ' = ' + (num * i));
+// }
+
+// let num = 2;
+// for(let i = 1; i <= 10; i++){
+//     console.log(num + ' x ' + i + ' = ' + (num * i));
+// }
+
+// let num = 9;
+// for(let i = 1; i <= 10; i++){
+//     console.log(num + ' x ' + i + ' = ' + (num * i));
+// }
+
+// let num = 8;
+// for(let i = 1; i <= 10; i++){
+//     console.log(num + ' x ' + i + ' = ' + (num * i));
+// }
+
+let num = 10;
+for(let i = 1; i <= 10; i++){
+    console.log(num + ' x ' + i + ' = ' + (num * i));
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
